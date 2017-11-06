a96adb615a87af0a395e131af62b9032dd7c050e
Sn0wFox <ruben.pericas@gmail.com>
2017-11-06 23:06:21 +0100

    Skel and jquery.panel are now bundled as vendors, and the main script is now part of index.js

--- src/index.js
+++ src/index.js
@@ -3,8 +3,107 @@
 // import './libs/js/ie/respond.min';
 
 // Libs
-// import * as $ from 'jquery';
-import 'skel.min';
-import 'main';
+import $ from 'jquery';
+import skel from 'skel.min';
 
-console.log('it works!');
+// Init page
+init();
+
+/**
+ * Initialize the page by adding breakpoints, events handlers and panel menu
+ * to the page.
+ */
+function init() {
+  skel
+    .breakpoints({
+      xlarge:	'(max-width: 1680px)',
+      large:	'(max-width: 1280px)',
+      medium:	'(max-width: 980px)',
+      small:	'(max-width: 736px)',
+      xsmall:	'(max-width: 480px)'
+    });
+
+  $(function() {
+
+    // Cache jquery request
+    let	$window = $(window),
+      $body = $('body'),
+      $wrapper = $('#page-wrapper'),
+      $banner = $('#banner'),
+      $header = $('#header');
+
+    // Disable animations/transitions until the page has loaded.
+    $body.addClass('is-loading');
+
+    $window.on('load', () => {
+      window.setTimeout(() => {
+        $body.removeClass('is-loading');
+      }, 100);
+    });
+
+    // Handle mobile size
+    if (skel.vars.mobile) {
+      $body.addClass('is-mobile');
+    }
+    else {
+      skel
+        .on('-medium !medium', () => {
+          $body.removeClass('is-mobile');
+        })
+        .on('+medium', () => {
+          $body.addClass('is-mobile');
+        });
+    }
+
+    // Fix: Placeholder polyfill.
+    $('form').placeholder();
+
+    // Prioritize "important" elements on medium.
+    skel.on('+medium -medium', () => {
+      $.prioritize(
+        '.important\\28 medium\\29',
+        skel.breakpoint('medium').active
+      );
+    });
+
+    // Initialize scrolly
+    $('.scrolly')
+      .scrolly({
+        speed: 1500,
+        offset: $header.outerHeight()
+      });
+
+    // Initialize panel menu
+    $('#menu')
+      .append('<a href="#menu" class="close"></a>')
+      .appendTo($body)
+      .panel({
+        delay: 500,
+        hideOnClick: true,
+        hideOnSwipe: true,
+        resetScroll: true,
+        resetForms: true,
+        side: 'right',
+        target: $body,
+        visibleClass: 'is-menu-visible'
+      });
+
+    // Initialize header
+    if (skel.vars.IEVersion < 9) {
+      $header.removeClass('alt');
+    }
+
+    if ($banner.length > 0 &&	$header.hasClass('alt')) {
+      $window.on('resize', () => { $window.trigger('scroll'); });
+      $banner.scrollex({
+        bottom:     $header.outerHeight() + 1,
+        terminate:	function() { $header.removeClass('alt'); },
+        enter:      function() { $header.addClass('alt'); },
+        leave:      function() { $header.removeClass('alt'); }
+      });
+    }
+
+  });
+
+
+}
