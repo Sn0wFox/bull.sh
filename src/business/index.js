e0eca95eb7eecf16ec4845fe2117d8295f9341cc
Sn0wFox <ruben.pericas@gmail.com>
2017-12-13 08:32:15 +0100

    'Business' is now 'Finances'

--- src/business/index.js
+++
@@ -1,8 +0,0 @@
-import $ from 'jquery';
-import {initMenu, initHeader} from '../commons/common';
-
-let $body = $('body');
-let $window = $(window);
-
-initMenu('#menu', $body);
-initHeader($('#bull-header'), $('#bull-article-header'), $window);
